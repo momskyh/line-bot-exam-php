554fdd2fa4f5164add021f61e3d985784d740475
momskyh <37992953+momskyh@users.noreply.github.com>
2018-05-09 16:54:58 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -7,23 +7,12 @@ $userId = 'Ud5680fffd4957a5bc2af997beabc72ba';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
-$headers = array('Authorization: Bearer ' . $access_token);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result;
-
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $response = $bot->getProfile($userId);
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
-    echo $profile;//['displayName'];
-    //echo $profile['pictureUrl'];
-    //echo $profile['statusMessage'];
+    echo $profile['displayName'];
+    echo $profile['pictureUrl'];
+    echo $profile['statusMessage'];
 }
