cebd51048da56083f9be0539d4779f10aa617bd1
momskyh <37992953+momskyh@users.noreply.github.com>
2018-05-09 13:59:19 +0700

    Update pushv2.php

--- pushv2.php
+++ pushv2.php
@@ -3,11 +3,12 @@
 $access_token = 'h6qzlIg7k7SdqLjiq4CCa+AuVQpCGoPF+cg78G+HlBrBH1v6u2D/SP6slIAsydQAqqUoFR3Z5XVRlh4CnTULSDEkZmeUwXHUhTcu2N+XzgUTQVWW4oOrlMQnoIORLgV6H52Ctn3cEuIOrPUJbXWDTQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '81723630c588835f06fbf9918436be6a';
 $idPush = 'Ud5680fffd4957a5bc2af997beabc72ba'
+  
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 
-//$response = $bot->pushMessage('U7ef7a449f2a5c2057eacfc02ba2eb286', $textMessageBuilder);
+//$response = $bot->pushMessage($idPush, $textMessageBuilder);
 $response = $bot->pushMessage($idPush, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
