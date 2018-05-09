75b312fe76434e3127793d604f576d76f92c1a85
momskyh <37992953+momskyh@users.noreply.github.com>
2018-05-09 14:03:28 +0700

    Update pushv2.php

--- pushv2.php
+++ pushv2.php
@@ -1,5 +1,5 @@
 <?php
-//require "vendor/autoload.php";
+require "vendor/autoload.php";
 $access_token = 'h6qzlIg7k7SdqLjiq4CCa+AuVQpCGoPF+cg78G+HlBrBH1v6u2D/SP6slIAsydQAqqUoFR3Z5XVRlh4CnTULSDEkZmeUwXHUhTcu2N+XzgUTQVWW4oOrlMQnoIORLgV6H52Ctn3cEuIOrPUJbXWDTQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '81723630c588835f06fbf9918436be6a';
 $idPush = 'Ud5680fffd4957a5bc2af997beabc72ba'
