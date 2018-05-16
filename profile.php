50ff6bd91780fe130a29ba355d82849bdf7821e6
momskyh <37992953+momskyh@users.noreply.github.com>
2018-05-16 10:04:20 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -15,6 +15,7 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
+echo $result['displayName'];
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
