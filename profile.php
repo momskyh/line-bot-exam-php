05dceb29aa04f3cb8342db6ca18a05aa042e4cf7
momskyh <37992953+momskyh@users.noreply.github.com>
2018-05-16 10:01:27 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -5,7 +5,16 @@ $access_token = 'h6qzlIg7k7SdqLjiq4CCa+AuVQpCGoPF+cg78G+HlBrBH1v6u2D/SP6slIAsydQ
 $channelSecret = '81723630c588835f06fbf9918436be6a';
 $userId = 'Ud5680fffd4957a5bc2af997beabc72ba';
 
+
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
+$headers = array('Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
