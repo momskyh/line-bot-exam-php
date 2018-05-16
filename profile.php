309c84369daa941d67f2ce0a3ed600940566018c
momskyh <37992953+momskyh@users.noreply.github.com>
2018-05-16 10:24:57 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -14,11 +14,8 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-$cc=$result['displayName'];
-echo $result;
 echo $result['userId'];
 echo $result['displayName'];
 echo $result[1:10];
-echo $cc;
 
 ?>
